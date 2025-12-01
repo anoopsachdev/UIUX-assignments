403ef49f509ba6ecf7aa433173c0ca0d4f0f5478
anoopsachdev <anoopsinghsachdev@gmail.com>
2025-12-01 14:57:15 +0530

    Removed prev_files folder (containing old js practice codes from 3rd/4th semester)

--- prev_files/Javascript/file2.js
+++
@@ -1,9 +0,0 @@
-// dates
-
-var x = new Date();
-
-console.log("Default view:", x)
-
-console.log("getTime() method", x.getTime())
-console.log("getFulYear() method",x.getFullYear())
-console.log("toLocaleTimeString() method",x.toLocaleTimeString())
--- prev_files/Javascript/file4.js
+++
@@ -1,3 +0,0 @@
-array = [1,2,3,4,54,70]
-console.log(array.join())
-console.log(typeof array.join())
